a0df4d64b05a60653e75af928191479c16224586
BelaBelphegor <tiboitel@student.42.fr>
2017-09-12 16:19:21 +0200

    adding ball js

---
+++ js/ball.js
@@ -0,0 +1,8 @@
+class Ball
+{
+    // Ici on ecrit les bailles pour ajouter des attributs
+    constructor()
+    {
+
+    }
+}
--- js/game.js
+++ js/game.js
@@ -2,6 +2,7 @@ class Game
 {
     constructor()
     {
-        this.c = new Coquette(this, "breakout-js", 1024, 760, "#FAD");
+        this.c = new Coquette(this, "breakout-js", 800, 600, "#1F084F");
+        // this.c.entities.create(Ball, { center: {x:32, y:484}, color:"#f07"});
     }
 }
