8d2639ceb186336bd8c5e622eef4a6e56294939f
madecreu <madecreu@student.42.fr>
2017-09-12 16:37:11 +0200

    Push du cul mais de meilleuyre qualitye

--- js/ball.js
+++ js/ball.js
@@ -1,13 +1,23 @@
 class Ball
 {
-    // Ici on ecrit les bailles pour ajouter des attributs
-    constructor()
-    {
-	this.speed = 3;
-	dir_x;
-	dir_y;
-	x;
-	y;
-	size;
-    }
+	// Ici on ecrit les bailles pour ajouter des attributs
+	constructor()
+	{
+		this.speed = 3;
+		this.dir_x = -1;
+		this.dir_y = 1;
+		this.x = 0;
+		this.y = 0;
+		this.size = 10;
+	}
+
+	constructor(x, y, size)
+	{
+		this.speed = 3;
+		this.dir_x = -1;
+		this.dir_y = 1;
+		this.x = x;
+		this.y = y;
+		this.size = size;	
+	}
 }
