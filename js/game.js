96ac7abc60d6118f252746e759f0cd9d8bda945c
madecreu <madecreu@student.42.fr>
2017-09-12 17:58:26 +0200

    Ca plante du cul

--- js/game.js
+++ js/game.js
@@ -4,6 +4,7 @@ class Game
     {
         this.c = new Coquette(this, "breakout-js", 800, 600, "#1F084F")
         this.push_entities(new Ball(400, 300, 25));
+	this.push_entities(new Player(this, 400, 20));
     }
 
     push_entities(object)
--- js/player.js
+++ js/player.js
@@ -2,9 +2,12 @@ class Player
 {
     constructor(game, x, y)
     {
-        this.c = game;
+        this.c = game.c;
         this._x = x;
         this._y = y;
+	this.heigth = 10;
+	this.width = 50;
+	this.speed = 5;
         // x, y, heigth, width
     }
 
@@ -15,26 +18,33 @@ class Player
 
     set x(x)
     {
-        if (endehorsdelecran)
-        {
-            direquex est egal a la taille de lecran
-        }
-        this._x = x;
+        if (this.x() < this.width - 1)
+		this._x = + this.width - 1;
+	else
+		this._x = x;
     }
 
     update()
-    {
-        // si la fleche est appuye 
+    { 
         if (this.c.inputter.isDown(this.c.inputter.LEFT_ARROW))
-		{
+	{
+		if (this.x() - this.speed < this.width)
+			this.x(this.width);
+		else
+			this.x(this.x() - this.speed);
         }
         if (this.c.inputter.isDown(this.c.inputter.LEFT_ARROW))
-		{
+	{
+		if (this.x() + this.speed > 799 - this.width)
+			this.x(799 - this.width);
+		else
+			this.x(this.x() + this.speed);
         }
     }
 
     draw(ctx)
     {
-        // regarder google canvas fill rect
+        ctx.fillStyle = "#F8EA50";
+	ctx.fillRect(this._x, this._y, this.width, this.heigth);
     }
 }
