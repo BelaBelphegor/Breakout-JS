6aebb84968ae65ffe1bb6230152b49a4ca4e5f50
madecreu <madecreu@student.42.fr>
2017-09-12 17:15:56 +0200

    Ca bouge putaing

--- js/ball.js
+++ js/ball.js
@@ -13,14 +13,19 @@ class Ball
 
 	update()
 	{
-
+		if (this.x + this.speed * this.dir_x > 799 - this.size / 2 || this.x + this.speed * this.dir_x < 0 + this.size / 2)
+			this.dir_x *= -1;
+		if (this.y + this.speed * this.dir_y > 599 - this.size / 2 || this.y + this.speed * this.dir_y < 0 + this.size / 2)
+			this.dir_y *= -1;
+		this.x += this.speed * this.dir_x;
+		this.y += this.speed * this.dir_y;
 	}
 
 	draw(ctx)
 	{
 		ctx.fillStyle = "#F05BD5";
 		ctx.beginPath();
-        ctx.arc(this.x, this.y, this.size / 2, 0, Math.PI * 2);
-        ctx.fill();
+        	ctx.arc(this.x, this.y, this.size / 2, 0, Math.PI * 2);
+        	ctx.fill();
 	}
 }
